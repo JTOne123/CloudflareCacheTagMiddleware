fb33ab00bdf32defb04faafc6a0032461e9c988d
Ashley Poole <ashley.poole@protolabs.co.uk>
2018-03-04 20:21:40 +0000

    Add example PS script to clear cache

---
+++ PurgeTagByHeaderExample.ps1
@@ -0,0 +1,19 @@
+﻿# User input
+$zoneDomainName = 'my-domain-name-here'
+$cacheTag = 'my-cache-tag'
+
+# User secrets
+$auth_email = "my-cloudflare-user@my-domain.com"
+$auth_key = "my-auth-key"
+
+$zonesApiUrl = "https://api.cloudflare.com/client/v4/zones"
+$headers = @{"X-Auth-Email"="$auth_email"; "X-Auth-Key"="$auth_key"; "Content-Type"="application/json"};
+ 
+$zones = Invoke-RestMethod -Uri "$zonesApiUrl/?name=$zoneDomainName" -Method Get -Headers $headers -ErrorAction Stop
+
+if ($zones.result.Count -ne 1)
+{
+    Throw "Lookup for $zoneDomainName zone found $($zones.result.Count) results. 1 result should of been found. Unable to purge cache."
+}
+
+Invoke-RestMethod -Uri "$zonesApiUrl/$($zones.result.Id)/purge_cache" -Method Delete -Headers $headers -Body $('{"tags":["' + $cacheTag + '"]}') -ErrorAction Stop
